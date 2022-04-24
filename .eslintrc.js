aa58549b02e00a9f5fed5d970c96c0bd70dfedef
mogi86 <nobuyuki.mogi@gmail.com>
2022-04-25 08:40:10 +0900

    add eslint rule & layouts/default

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,13 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    'vue/multi-word-component-names': 'off'
+    'vue/multi-word-component-names': 'off',
+    'vue/singleline-html-element-content-newline': [
+      'error',
+      {
+        ignores: ['NuxtLink']
+      }
+    ],
+    'vue/no-multiple-template-root': 'off'
   }
 }
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink> |
+      <NuxtLink to="/about">About</NuxtLink> |
+    </nav>
+    <Nuxt />
+  </div>
+</template>
